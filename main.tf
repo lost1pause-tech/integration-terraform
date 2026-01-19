fa862a060d468ad6e1bd169d45ce36624a1c8a88
arpitha <lost1pause@gmail.com>
2026-01-18 22:30:09 -0800

    terraform files

---
+++ main.tf
@@ -0,0 +1,97 @@
+resource "azurerm_resource_group" "arpitharg" {
+name = var.name
+location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+name = var.vnet_name
+location = azurerm_resource_group.arpitharg.location
+resource_group_name = azurerm_resource_group.arpitharg.name
+address_space = var.vnet_address
+}
+
+resource "azurerm_subnet" "subnet" {
+name = var.subnet_name
+resource_group_name = azurerm_resource_group.arpitharg.name
+virtual_network_name = azurerm_virtual_network.vnet.name
+address_prefixes = var.subnet_address_prefixes
+}
+
+resource "azurerm_network_security_group" "nsg" {
+name = var.nsg_name
+location = azurerm_resource_group.arpitharg.location
+resource-group_name = azurerm_resource_group.arpitha.name
+
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_assocation" "subnet_nsg" {
+subnet_id = azurerm_subnet.subnet.id
+network_security_group_id =azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_public_ip" "pip" {
+name = var.pip_name
+location = azurerm_resource_group.arpitharg.location
+resource_group_name = azurerm_resource_group.arpitharg.name
+allocation_method = "Static"
+sku = "Standard"
+}
+
+resource "azurerm_network_interface" "nic" {
+name = var.nic_name
+location = azurerm_resource_group.arpitharg.location
+resource_group_name = azurerm_resource_group.arpitharg.name
+
+ip_configuration {
+name = "internal"
+subnet_id = azurerm_subnet.subnet.id
+private_ip_address_allocation = "Dynamic"
+public_ip_address_id = azurerm_public_ip.pip.id
+}
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+name = var.vm_name
+location = azurerm_resource_group.arpitharg.name
+resource_group_name = azurerm_resource_group.arpitharg.name
+size = var.vm_size
+
+admin_username = var.adminuser
+
+network_interface_id = [
+azurerm_resource_network_interface.ic.id ]
+
+admin_ssh_key {
+username = var.adminuser
+public_key = var.ssh_public_key
+}
+
+os_disk {
+name = "$(var.vm_name)-osdisk"
+caching = "ReadWrite"
+stroage_account_type = "Standard_LRS"
+}
+
+source_image_reference {
+publisher = "Cononical"
+offer = "0001-com-ubuntu-server-jammy"
+sku = "22_04-lts-gen2"
+version = "latest"
+}
+
+disable_password_authentication = true
+
+}
+
