05d347edbd85782e5bbef15fcd787be45a345c74
arpitha <lost1pause@gmail.com>
2026-01-18 23:15:11 -0800

    second terraform commit

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 resource "azurerm_resource_group" "arpitharg" {
-name = var.name
-location = var.location
+  name     = var.name
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-name = var.vnet_name
-location = azurerm_resource_group.arpitharg.location
-resource_group_name = azurerm_resource_group.arpitharg.name
-address_space = var.vnet_address
+  name                = var.vnet_name
+  location            = azurerm_resource_group.arpitharg.location
+  resource_group_name = azurerm_resource_group.arpitharg.name
+  address_space       = var.vnet_address
 }
 
 resource "azurerm_subnet" "subnet" {
-name = var.subnet_name
-resource_group_name = azurerm_resource_group.arpitharg.name
-virtual_network_name = azurerm_virtual_network.vnet.name
-address_prefixes = var.subnet_address_prefixes
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.arpitharg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_address_prefixes
 }
 
 resource "azurerm_network_security_group" "nsg" {
-name = var.nsg_name
-location = azurerm_resource_group.arpitharg.location
-resource-group_name = azurerm_resource_group.arpitha.name
+  name                = var.nsg_name
+  location            = azurerm_resource_group.arpitharg.location
+  resource_group_name = azurerm_resource_group.arpitharg.name
 
 
   security_rule {
@@ -36,62 +36,63 @@ resource-group_name = azurerm_resource_group.arpitha.name
   }
 }
 
-resource "azurerm_subnet_network_security_group_assocation" "subnet_nsg" {
-subnet_id = azurerm_subnet.subnet.id
-network_security_group_id =azurerm_network_security_group.nsg.id
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_public_ip" "pip" {
-name = var.pip_name
-location = azurerm_resource_group.arpitharg.location
-resource_group_name = azurerm_resource_group.arpitharg.name
-allocation_method = "Static"
-sku = "Standard"
+  name                = var.pip_name
+  location            = azurerm_resource_group.arpitharg.location
+  resource_group_name = azurerm_resource_group.arpitharg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "nic" {
-name = var.nic_name
-location = azurerm_resource_group.arpitharg.location
-resource_group_name = azurerm_resource_group.arpitharg.name
+  name                = var.nic_name
+  location            = azurerm_resource_group.arpitharg.location
+  resource_group_name = azurerm_resource_group.arpitharg.name
 
-ip_configuration {
-name = "internal"
-subnet_id = azurerm_subnet.subnet.id
-private_ip_address_allocation = "Dynamic"
-public_ip_address_id = azurerm_public_ip.pip.id
-}
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-name = var.vm_name
-location = azurerm_resource_group.arpitharg.name
-resource_group_name = azurerm_resource_group.arpitharg.name
-size = var.vm_size
+  name                = var.vm_name
+  location            = azurerm_resource_group.arpitharg.location
+  resource_group_name = azurerm_resource_group.arpitharg.name
+  size                = var.vm_size
 
-admin_username = var.adminuser
+  admin_username = var.adminuser
 
-network_interface_id = [
-azurerm_resource_network_interface.ic.id ]
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
 
-admin_ssh_key {
-username = var.adminuser
-public_key = var.ssh_public_key
-}
+  admin_ssh_key {
+    username   = var.adminuser
+    public_key = var.ssh_public_key
+  }
 
-os_disk {
-name = "$(var.vm_name)-osdisk"
-caching = "ReadWrite"
-stroage_account_type = "Standard_LRS"
-}
+  os_disk {
+    name                 = "${var.vm_name}-osdisk"
+    storage_account_type = "Standard_LRS"
 
-source_image_reference {
-publisher = "Cononical"
-offer = "0001-com-ubuntu-server-jammy"
-sku = "22_04-lts-gen2"
-version = "latest"
-}
+  }
 
-disable_password_authentication = true
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  disable_password_authentication = true
 
 }
 
--- var.tf
+++ var.tf
@@ -1,64 +1,64 @@
 variable "name" {
-type = string
-default = "arpitharg"
+  type    = string
+  default = "arpitharg"
 }
 
 variable "location" {
-type = string
-default = "eastus"
+  type    = string
+  default = "eastus"
 }
 
 variable "vnet_name" {
-type = string
-default = arpitha-vnet
+  type    = string
+  default = "arpitha-vnet"
 }
 
 variable "vnet_address" {
-type = list(string)
-default = ["10.0.0.0/16"]
+  type    = list(string)
+  default = ["10.0.0.0/16"]
 }
 
 variable "subnet_name" {
-type = string
-default = "arpitha-subnet"
+  type    = string
+  default = "arpitha-subnet"
 }
 
 variable "subnet_address_prefixes" {
-type = list(string)
-default = ["10.0.1.0/24"]
+  type    = list(string)
+  default = ["10.0.1.0/24"]
 }
 
 variable "nsg_name" {
-type = string
-default = "arpitha-nsg"
+  type    = string
+  default = "arpitha-nsg"
 }
 
-varaible "pip_name" {
-type = string
-default = "arpitha-public-ip"
+variable "pip_name" {
+  type    = string
+  default = "arpitha-public-ip"
 }
 
-varaivle "nic_name" {
-type = string
-default = "arpitha-nic"
+variable "nic_name" {
+  type    = string
+  default = "arpitha-nic"
 }
 
-varaible "vm_name" {
-type = string
-default = "linux_vm_arpitha_vm1"
-discrpition = "linux vm name"
+variable "vm_name" {
+  type        = string
+  default     = "linux_vm_arpitha_vm1"
+  description = "linux vm name"
 }
 
 variable "vm_size" {
-type = string
-default = "Standard_DC1s_v3"
-discrpition = "vm size"
+  type        = string
+  default     = "Standard_DC1s_v3"
+  description = "vm size"
 }
 
-varaible "adminuser" {
-type = string
-default = "azureuser"
-discrpition = "admin user"
+variable "adminuser" {
+  type        = string
+  default     = "azureuser"
+  description = "admin user"
 }
 
 variable "ssh_public_key" {
